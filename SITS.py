77bda81a988ed201db5a2eae08c9dfbc99a85a4a
osekenj <kenji.ose@ec.europa.eu>
2024-06-05 17:15:52 +0200

    save bug

--- SITS.py
+++ SITS.py
@@ -11,6 +11,10 @@ import planetary_computer as pc
 import odc
 from odc.geo.geobox import GeoBox
 from odc.stac import configure_rio, stac_load
+# Geospatial librairies
+import rasterio
+from rasterio.features import rasterize
+
 
 class Csv2gdf:
 
@@ -64,9 +68,13 @@ class Csv2gdf:
         xmin, ymin, xmax, ymax = row.geometry.bounds
         return box(xmin, ymin, xmax, ymax)
 
+
 class StacAttack:
 
-    def __init__(self, provider='mpc', collection='sentinel-2-l2a'):
+    def __init__(self, provider='mpc',
+                       collection='sentinel-2-l2a',
+                       bands=['B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'SCL']
+                ):
         self.prov_stac = {'mpc':{'stac': 'https://planetarycomputer.microsoft.com/api/stac/v1',
                                  'coll': collection,
                                  'modifier': pc.sign_inplace,
@@ -78,6 +86,28 @@ class StacAttack:
                          }
         self.stac = self.prov_stac[provider]
         self.catalog = Client.open(self.stac['stac'], modifier=self.stac['modifier'])
+        self.bands = bands
+        self.stac_conf = {'chunks_size':612, 'dtype':"uint16", 'nodata':0}
+
+    def __items_to_array(self):
+        arr = stac_load(self.items,
+                        bands=self.bands,
+                        groupby="solar_day",
+                        chunks={"x": self.stac_conf['chunks_size'], 
+                                "y": self.stac_conf['chunks_size']},
+                        patch_url=self.stac['patch_url'],
+                        dtype=self.stac_conf['dtype'],
+                        nodata=self.stac_conf['nodata'],
+                        geobox=self.geobox
+                        )
+        return arr
+
+    def __choose_array(self, array_type):
+        if array_type == 'image':
+            e_array = self.i_array
+        elif array_type == 'patch':
+            e_array = self.p_array
+        return e_array
 
     def searchItems(self, bbox_latlon, date_start='2023-01', date_end='2023-12', **kwargs):
         self.startdate = date_start
@@ -90,57 +120,28 @@ class StacAttack:
                                    )
         self.items = list(query.items())
 
-    def loadPatches(self, bbox, resolution, dimx, dimy,
-                    bands=['B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 
-                           'B8A', 'B11', 'B12', 'SCL'],
-                    crs_out=3035, chunks_size=612, dtype="uint16", nodata=0):
+    def loadPatches(self, bbox, dimx=5, dimy=5, resolution=10, crs_out=3035):
 
         shape = (dimx, dimy)
-
         self.def_geobox(bbox, crs_out, resolution, shape)
 
-        self.p_array = stac_load(self.items,
-                               bands=bands,
-                               groupby="solar_day",
-                               chunks={"x": chunks_size, "y": chunks_size},
-                               patch_url=self.stac['patch_url'],
-                               dtype=dtype,
-                               nodata=nodata,
-                               geobox=self.geobox
-                     )
+        self.patch = self.__items_to_array()
 
-    def loadImgs(self, bbox, resolution=10,
-                 bands=['B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 
-                        'B8A', 'B11', 'B12', 'SCL'],
-                 crs_out=3035, chunks_size=612, dtype="uint16", nodata=0
-                 ):
+    def loadImgs(self, bbox, resolution=10, crs_out=3035):
 
         self.def_geobox(bbox, crs_out, resolution)
 
-        self.i_array = stac_load(self.items,
-                               bands=bands,
-                               groupby="solar_day",
-                               chunks={"x": chunks_size, "y": chunks_size},
-                               patch_url=self.stac['patch_url'],
-                               dtype=dtype,
-                               nodata=nodata,
-                               geobox=self.geobox
-                     )
+        self.image = self.__items_to_array()
 
-    def __choose_array(self, array_type):
-        if array_type == 'image':
-            e_array = self.i_array
-        elif array_type == 'patch':
-            e_array = self.p_array
-        return e_array
-
-    def to_df(self, array_type):
-        e_array = self.__choose_array(array_type)
+    def __to_df(self, array_type):
+        #e_array = self.__choose_array(array_type)
+        e_array = getattr(self, array_type)
         array_trans = e_array.transpose('time', 'y', 'x')
         df = array_trans.to_dataframe()
         return df
 
-    def to_csv(self, df, outdir, gid=None):
+    def to_csv(self, array_type, outdir, gid=None):
+        df = self.__to_df(array_type)
         df = df.reset_index()
         df['ID'] = df.index
         if gid is not None:
@@ -148,20 +149,12 @@ class StacAttack:
         else:
             df.to_csv(os.path.join(outdir, f'id_none_{array_type}.csv'))
 
-    def old_to_csv(self, array_type, gid, outdir):
-        """deprecated"""
-        self.df = self.df.reset_index()
-        self.df['ID'] = self.df.index
-        self.df.insert(0, "station", gid)
-        self.df['date'] = pd.to_datetime(self.df['time']).dt.date
-        self.df.to_csv(os.path.join(outdir, f'station_{gid}_{array_type}.csv'))
-
     def to_nc(self, array_type, gid, outdir):
-        e_array = self.__choose_array(array_type)
+        #e_array = self.__choose_array(array_type)
+        e_array = getattr(self, array_type)
         e_array.to_netcdf(f"{outdir}/S2_fid-{gid}_{array_type}_{self.startdate}-{self.enddate}.nc")
 
     def def_geobox(self, bbox, crs_out, resolution, shape=None):
-
         crs = CRS.from_epsg(crs_out)
         if shape is not None:
             # size in pixels of input bbox
@@ -184,3 +177,36 @@ class StacAttack:
         self.geobox = GeoBox.from_bbox(odc.geo.geom.BoundingBox(*self.newbbox),
                                        crs=crs,
                                        resolution=resolution)
+
+    def old_to_csv(self, array_type, gid, outdir):
+        """deprecated"""
+        self.df = self.df.reset_index()
+        self.df['ID'] = self.df.index
+        self.df.insert(0, "station", gid)
+        self.df['date'] = pd.to_datetime(self.df['time']).dt.date
+        self.df.to_csv(os.path.join(outdir, f'station_{gid}_{array_type}.csv'))
+
+
+class Labels:
+    def __init__(self, geolayer):
+        if isinstance(geolayer, pd.core.frame.DataFrame):
+            self.gdf = geolayer.copy()
+        else:
+            self.gdf = gpd.read_file(geolayer)
+        #self.geom = self.gdf.geometry
+
+    def to_raster(self, id_field, geobox, filename, outdir, crs='EPSG:3035', driver="GTiff"):
+        shapes = ((geom, value) for geom, value in zip(self.gdf.geometry, self.gdf[id_field]))
+        rasterized = rasterize(#[(self.geom.iloc[0], 255)],
+                               shapes, 
+                               out_shape=(geobox.height, geobox.width),
+                               transform=geobox.transform,
+                               fill=0,
+                               all_touched=False,
+                               dtype='uint8')
+
+        # Write the rasterized feature to a new raster file
+        with rasterio.open(os.path.join(outdir, filename), 'w', driver=driver, crs=crs,
+                           transform=geobox.transform, dtype=rasterio.uint8, count=1, 
+                           width=geobox.width, height=geobox.height) as dst:
+            dst.write(rasterized, 1)
