2bd7bf6780b5958aa9810013e2119a16bbfdb257
osekenj <kenji.ose@ec.europa.eu>
2025-01-08 17:35:03 +0100

    start gif module

--- sits/export.py
+++ sits/export.py
@@ -1,10 +1,26 @@
+import xarray as xr
+import dask.array as dask_array
 import geogif
 
-gif_img = geogif.dgif(composites, fps=8).compute()
-gif_img
 
-# as file
-gif_bytes = geogif.dgif(composites, fps=8, bytes=True, robust=True).compute()
+class Sits_ds:
+    """handle xarray.dataset"""
 
-with open("example.gif", "wb") as f:
-    f.write(gif_bytes)
+    def __init__(self, nc_path):
+        self.ds = xr.open_dataset(nc_path, engine="netcdf4")
+
+    def ds2da(self, keep_bands=['B04', 'B03', 'B02']):
+        """to fill"""
+        sel_ds = self.ds[keep_bands]
+        self.da = sel_ds.to_array(dim='band')
+        self.da = self.da.transpose('time', 'band', 'x', 'y')
+
+    def export2gif(self, imgfile=None, fps=8, robust=True): #, bytes=True, ):
+        """to fill"""
+        if isinstance(self.da.data, dask_array.Array):
+            self.gif = geogif.dgif(self.da, fps=fps, robust=robust).compute()
+        else:
+            self.gif = geogif.gif(self.da, fps=fps, robust=robust)
+#        if imgfile is not None:
+#            with open(imgfile, "wb") as f:
+#                f.write(gif_img)
