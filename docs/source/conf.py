a6ac5d5cc56eda1389715a6301c6457b6190fab7
osekenj <kenji.ose@ec.europa.eu>
2025-06-26 17:43:59 +0200

    minor fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ release = '0.6.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.napoleon',
-              'sphinx.ext.autodoc', 
+              'sphinx.ext.autodoc',
               'sphinx.ext.autosummary',]
 
 templates_path = ['_templates']
